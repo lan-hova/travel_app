0aca29c3759b4a5814473dd7e2b716db76f510e8
Hồ Văn Lân <holan2k@gmail.com>
2023-04-13 16:07:25 +0700

    Update Home_Screen

--- lib/representation/screens/home_screen.dart
+++ lib/representation/screens/home_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:travel_app/core/constants/dimension_constants.dart';
+import 'package:travel_app/helpers/assets_helper.dart';
 import 'package:travel_app/representation/widgets/app_bar_container.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,12 +15,138 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Widget _buildItemCategory(Widget icon, Color color, Function() onTap, String title){
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: kMediumPadding),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(kItemPadding),
+              color: color.withOpacity(0.2)
+            ),
+            child: icon,
+          ),
+          SizedBox(
+            height: kItemPadding,
+          ),
+          Text(title)
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return AppBarContainer(
-      child: Container(
-        color: Colors.blue,
+      // titleString: 'Home',
+      // implementLeading: true,
+      // implementTrailing: true,
+
+      title: Padding(
+        padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Hi Jame !',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                ),
+                SizedBox(
+                  height: kMediumPadding,
+                ),
+                Text('How do you feel to day?', style: TextStyle(fontSize: 12),)
+              ],
+            ),
+            Spacer(),
+            Icon(FontAwesomeIcons.bell, size: kDefaultIconSize,),
+            SizedBox(width: kTopPadding,),
+            Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(kItemPadding),
+                color: Colors.white
+              ),
+              padding: EdgeInsets.only(left: kMinPadding, right: kMinPadding, top: kMinPadding),
+              child: Image.asset(AssetsHelper.person,)
+            )
+
+          ],
+        ),
       ),
+      child: Column(
+          children: [
+            TextField(
+              // showCursor: false,
+              enabled: false,
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                hintText: 'What is your favorite destination ?',
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(kTopPadding),
+                  child: Icon(
+                    FontAwesomeIcons.magnifyingGlass,
+                    color: Colors.black,
+                    size: kDefaultIconSize,
+                  ),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(kItemPadding)
+
+                ),
+                contentPadding: EdgeInsets.symmetric(horizontal: kItemPadding),
+                fillColor: Colors.white,
+                filled: true,
+              ),
+            ),
+            SizedBox(
+              height: kDefaultPadding,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: _buildItemCategory(
+                  Image.asset(AssetsHelper.icoHotel,
+                  width: kBottomBarIconSize,
+                  height: kBottomBarIconSize,
+                  ),
+                  Colors.pinkAccent,
+                  () {},
+                  'Hotels'),
+                ),
+                SizedBox(width: kDefaultPadding,),
+                Expanded(
+                  child: _buildItemCategory(
+                  Image.asset(AssetsHelper.icoPlane,                  
+                  width: kBottomBarIconSize,
+                  height: kBottomBarIconSize,
+                  ),
+                  Colors.blueAccent,
+                  () {},
+                  'Flights'),
+                ),
+                SizedBox(width: kDefaultPadding,),
+                Expanded(
+                  child: _buildItemCategory(
+                  Image.asset(AssetsHelper.icoHotelPlane,
+                  width: kBottomBarIconSize,
+                  height: kBottomBarIconSize,
+                  ),
+                  Colors.yellowAccent,
+                  () {},
+                  'All'),
+                )
+              ],
+            )
+
+          ],
+        ),
     );
   }
 }
--- lib/representation/widgets/app_bar_container.dart
+++ lib/representation/widgets/app_bar_container.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:travel_app/core/constants/color_palatte.dart';
 import 'package:travel_app/core/constants/dimension_constants.dart';
 
 class AppBarContainer extends StatelessWidget {
-  const AppBarContainer({required this.child ,super.key});
+  const AppBarContainer(
+      {required this.child,
+      this.title,
+      this.titleString,
+      this.implementLeading = false,
+      this.implementTrailing = false,
+      super.key});
   final Widget child;
+  final Widget? title;
+  final String? titleString;
+  final bool implementLeading;
+  final bool implementTrailing;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +32,51 @@ class AppBarContainer extends StatelessWidget {
               elevation: 0,
               toolbarHeight: 90,
               backgroundColor: ColorPalette.backgroundScaffoldColor,
+              title: title ??
+                  Row(
+                    children: [
+                      if (implementLeading)
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(kDefaultPadding),
+                              ),
+                              color: Colors.white),
+                          padding: EdgeInsets.all(kItemPadding),
+                          child: Icon(
+                            FontAwesomeIcons.arrowLeft,
+                            color: Colors.black,
+                            size: kDefaultIconSize,
+                          ),
+                        ),
+                      Expanded(
+                          child: Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              titleString ?? '',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 24),
+                            )
+                          ],
+                        ),
+                      )),
+                      if (implementTrailing)
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(kDefaultPadding),
+                            ),
+                            color: Colors.white),
+                        padding: EdgeInsets.all(kItemPadding),
+                        child: Icon(
+                          FontAwesomeIcons.bars,
+                          color: Colors.black,
+                          size: kDefaultIconSize,
+                        ),
+                      ),
+                    ],
+                  ),
               flexibleSpace: Stack(
                 children: [
                   Container(
